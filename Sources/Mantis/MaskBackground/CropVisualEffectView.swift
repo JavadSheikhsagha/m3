a3b884908b839ef5ba51f323f209433e4b20813a
Javad Sheikhsagha <62242276+JavadSheikhsagha@users.noreply.github.com>
2023-02-09 12:04:20 +0330

    wad
    awdadwdwd

--- Sources/Mantis/MaskBackground/CropVisualEffectView.swift
+++ Sources/Mantis/MaskBackground/CropVisualEffectView.swift
@@ -42,13 +42,13 @@ class CropMaskVisualEffectView: UIVisualEffectView, CropMaskProtocol {
     static func getEffect(byType type: CropMaskVisualEffectType) -> (UIVisualEffect?, UIColor) {
         switch type {
         case .blurDark:
-            return (UIBlurEffect(style: .dark), .clear)
+            return (nil, UIColor.black.withAlphaComponent(0.35))
         case .dark:
-            return (nil, UIColor.black.withAlphaComponent(0.75))
+            return (nil, UIColor.black.withAlphaComponent(0.35))
         case .light:
             return (nil, UIColor.black.withAlphaComponent(0.35))
         case .none:
-            return (nil, .black)
+            return (nil, UIColor.black.withAlphaComponent(0.35))
         }
     }
     
